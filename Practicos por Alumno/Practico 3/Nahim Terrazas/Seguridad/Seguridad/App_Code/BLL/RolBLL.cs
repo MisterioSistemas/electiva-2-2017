c9ad1ab12c6b1c8059d3ebdcfcc5aa4834011c57
nahimdhaney <nahimdhaney@hotmail.com>
2017-10-13 18:15:00 -0400

    Add files via upload
    Proyecto 3 - Web2

---
+++ Practicos por Alumno/Practico 3/Nahim Terrazas/Seguridad/Seguridad/App_Code/BLL/RolBLL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Seguridad.App_Code.BLL
+{
+    public class RolBLL
+    {
+        public RolBLL()
+        {  }
+        public static List<Rol> SelectAll()
+        {
+            RolDSTableAdapters.RolesTableAdapter =  
+        }
+    }
+}
---
+++ Practicos por Alumno/Practico 3/Nahim Terrazas/Seguridad/Seguridad/App_Code/DTO/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    public class Rol
+    {
+        public int ID { get; set; }
+        public string nombre { get; set; }
+
+    }
